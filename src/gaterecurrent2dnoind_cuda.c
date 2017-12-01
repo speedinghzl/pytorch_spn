1b9e103098bb991d4a25d4b983baab48c0973b0b
sifeil <sifeil@sifeils-MacBook-Pro.local>
2017-11-30 16:52:16 -0800

    add stdbool.h for kernel.cu

--- src/gaterecurrent2dnoind_cuda.c
+++ src/gaterecurrent2dnoind_cuda.c
@@ -1,6 +1,7 @@
 // gaterecurrent2dnoind_cuda.c
 #include <THC/THC.h>
 #include <math.h>
+#include <stdbool.h>
 #include "gaterecurrent2dnoind_cuda.h"
 #include "cuda/gaterecurrent2dnoind_kernel.h"
 
